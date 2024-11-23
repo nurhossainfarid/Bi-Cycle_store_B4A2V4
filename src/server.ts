c53099450f10aa594cb941b6d3774f9e8d081713
nurhossainfarid <faahsan1516@gmail.com>
2024-11-23 23:28:55 +0600

    setup project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
+import mongoose from "mongoose";
 import app from "./app";
+import config from "./app/config";
 const port = 4000
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(`My app is listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
